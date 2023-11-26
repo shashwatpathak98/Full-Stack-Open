c8376178e8ee05c250f1c11819fe1336eb27bec3
Shashwat Pathak <shashwatpathak18@gmail.com>
2023-11-27 01:03:24 +0530

    feat: add new component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,26 @@
 import { useState } from "react";
 
+const Statistics = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const { good, neutral, bad } = props;
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <div>
+        <div>good {good}</div>
+        <div>neutral {neutral}</div>
+        <div>bad {bad}</div>
+        <div>all {good + neutral + bad}</div>
+        <div>
+          average{" "}
+          {(good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad) || 0}
+        </div>
+        <div>positive {(good / (good + neutral + bad)) * 100 || 0} %</div>
+      </div>
+    </div>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -13,18 +34,7 @@ const App = () => {
         <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
         <button onClick={() => setBad(bad + 1)}>bad</button>
       </div>
-      <h2>Statistics</h2>
-      <div>
-        <div>good {good}</div>
-        <div>neutral {neutral}</div>
-        <div>bad {bad}</div>
-        <div>all {good + neutral + bad}</div>
-        <div>
-          average{" "}
-          {(good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad) || 0}
-        </div>
-        <div>positive {(good / (good + neutral + bad)) * 100 || 0} %</div>
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
