e380c1e746f64d07faade15b4c04d51ac54420cf
Shashwat Pathak <shashwatpathak18@gmail.com>
2023-12-10 23:28:15 +0530

    feat: added exercise 2.4

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -46,7 +46,7 @@ const App = () => {
     },
   ];
 
-  return courses.map((course) => <Course course={course} />);
+  return courses.map((course) => <Course key={course.id} course={course} />);
 };
 
 export default App;
