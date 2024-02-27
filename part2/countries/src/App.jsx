e2b2703d56ac2fd1c98bff5e4125f1e1db6d646a
Shashwat Pathak <shashwatpathak18@gmail.com>
2024-02-27 20:52:38 +0530

    Added exercise 2.18

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,37 +1,52 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import { getAllCountries } from "./services/countries";
+import { getAllCountries, getCountryByName } from "./services/countries";
 
-function Countries({ countryList, country }) {
-  let countryToShow = [];
+function CountryDetails({ name }) {
+  const [details, setDetails] = useState(null);
 
-  for (let i = 0; i < countryList.length; i++) {
-    // If we find the exact match  we push that to the countryToShow array
-    if (countryList[i].toLowerCase().trim() === country.toLowerCase().trim()) {
-      countryToShow.push(countryList[i]);
-    }
-  }
+  useEffect(() => {
+    getCountryByName(name).then((det) => setDetails(det.data));
+  }, [name]);
+  //console.log(details);
 
-  // we find the partial match , as we don't find the exact match
-  if (countryToShow.length === 0) {
-    for (let i = 0; i < countryList.length; i++) {
-      if (
-        country &&
-        countryList[i]
-          .toLowerCase()
-          .trim()
-          .includes(country.toLowerCase().trim())
-      ) {
-        countryToShow.push(countryList[i]);
-      }
-    }
+  if (details) {
+    let langs = details.languages;
+    //console.log(langs);
+
+    var langArr = Object.values(langs);
+
+    //console.log(myArr);
   }
 
   return (
     <div>
+      <h1> {details && details.name.common} </h1>
+
+      <span> capital {details && details.capital[0]}</span>
+      <br />
+      <span> area {details && details.area}</span>
+
+      <h3>languages</h3>
+      {details && (
+        <ul>
+          {langArr.map((lang) => (
+            <li>{lang}</li>
+          ))}
+        </ul>
+      )}
+
+      <p>{details && <img src={details.flags.png} alt={`${name} flag`} />}</p>
+    </div>
+  );
+}
+
+function Countries({ countryList }) {
+  return (
+    <div>
       <span>
-        {countryToShow && countryToShow.length < 10
-          ? countryToShow.map((country) => <div key={country}>{country}</div>)
+        {countryList && countryList.length < 10
+          ? countryList.map((country) => <div key={country}>{country}</div>)
           : "Too many countries"}
       </span>
     </div>
@@ -54,13 +69,37 @@ function App() {
   const countryList = countries.map((country) => country.name.common);
   // console.log(countryList);
 
+  let countryToShow = [];
+
+  // If we find the exact match  we push that to the countryToShow array
+  const exactMatch = countryList.find(
+    (countryName) =>
+      countryName.toLowerCase().trim() === country.toLowerCase().trim()
+  );
+  if (exactMatch) {
+    countryToShow.push(exactMatch);
+  }
+
+  // we find the partial match , as we don't find the exact match
+  if (countryToShow.length === 0) {
+    const partialMatch = countryList.filter((countryName) =>
+      countryName.toLowerCase().trim().includes(country.toLowerCase().trim())
+    );
+
+    countryToShow = [...partialMatch];
+  }
+
   return (
     <div>
       <span>
         find countries <input type="text" onChange={handleInputChange} />
       </span>
       <div>
-        <Countries countryList={countryList} country={country} />
+        {countryToShow.length === 1 ? (
+          <CountryDetails name={countryToShow[0]} />
+        ) : (
+          country && <Countries countryList={countryToShow} />
+        )}
       </div>
     </div>
   );
