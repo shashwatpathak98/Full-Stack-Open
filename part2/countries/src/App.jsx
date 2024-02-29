8e2830c1cb84faf81c198aaaf0b0ed2c3a5594c0
Shashwat Pathak <shashwatpathak18@gmail.com>
2024-02-29 20:31:12 +0530

    Added exercise 2.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,6 +1,10 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import { getAllCountries, getCountryByName } from "./services/countries";
+import {
+  getAllCountries,
+  getCountryByName,
+  getCountryWeather,
+} from "./services/countries";
 
 function CountryDetails({ name }) {
   const [details, setDetails] = useState(null);
@@ -19,25 +23,69 @@ function CountryDetails({ name }) {
     //console.log(myArr);
   }
 
+  if (!details) return <></>;
+
   return (
     <div>
-      <h1> {details && details.name.common} </h1>
+      <h1> {details.name.common} </h1>
 
-      <span> capital {details && details.capital[0]}</span>
+      <span> capital {details.capital[0]}</span>
       <br />
-      <span> area {details && details.area}</span>
+      <span> area {details.area}</span>
 
       <h3>languages</h3>
-      {details && (
+      {
         <ul>
           {langArr.map((lang) => (
             <li key={Math.random() * 1000}>{lang}</li>
           ))}
         </ul>
-      )}
+      }
 
-      <p>{details && <img src={details.flags.png} alt={`${name} flag`} />}</p>
+      <p>{<img src={details.flags.png} alt={`${name} flag`} />}</p>
+      <h3> Weather in {details.capital[0]}</h3>
+      <div>
+        <CountryWeather
+          lat={details.capitalInfo.latlng[0]}
+          lng={details.capitalInfo.latlng[1]}
+        />
+      </div>
     </div>
+  );
+}
+
+function CountryWeather({ lat, lng }) {
+  const [weatherDetails, setWeatherDetails] = useState(null);
+
+  useEffect(() => {
+    getCountryWeather(lat, lng).then((weather) =>
+      setWeatherDetails(weather.data)
+    );
+  }, [lat, lng]);
+
+  if(weatherDetails){
+    let displayTemeperature = weatherDetails.main
+    var tempArr = Object.values(displayTemeperature)
+    let displayIcon = weatherDetails.weather[0]
+    var iconArr = Object.values(displayIcon)
+    let windObj = weatherDetails.wind
+    var windArr = Object.values(windObj)
+    //console.log(windArr);
+  }
+
+  //console.log(weatherDetails);
+
+  if(!weatherDetails) return <></>
+  return (
+  <div>
+     temperature {(tempArr[0] - 273.15).toFixed(2)} Celcius
+     <div>
+     <img src={`https://openweathermap.org/img/wn/${iconArr[3]}@2x.png`} alt="image of weather" />
+     </div>
+     <div>
+      wind {windArr[0]} m/s
+     </div>
+  </div>
   );
 }
 
--- part2/countries/src/services/countries.js
+++ part2/countries/src/services/countries.js
@@ -10,4 +10,11 @@ const getCountryByName = (country) => {
   );
 };
 
-export { getAllCountries, getCountryByName };
+const getCountryWeather = (lat, long) => {
+  const api_key = import.meta.env.VITE_WEATHER_KEY;
+  return axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api_key}`
+  );
+};
+
+export { getAllCountries, getCountryByName, getCountryWeather };
